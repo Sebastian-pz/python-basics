21bd1e420c6ae344c38fdd3fcc3597b0cb318b32
Sebastian Perez <spezuluaga@gmail.com>
2023-03-04 18:19:31 -0500

    modules

---
+++ modules/main.py
@@ -0,0 +1,10 @@
+import datetime
+from functions import sum, hello
+
+# Desesctructurado
+from datetime import timedelta
+
+print(datetime.date.today())
+
+# Minutos a horas
+print(datetime.timedelta(minutes=90))
